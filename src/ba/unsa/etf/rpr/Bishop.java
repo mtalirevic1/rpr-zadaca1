b265fa8cf76e54577a3c8e612caeac33b27bdd53
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-07 23:43:07 +0100

    Implementirana klasa Bishop

---
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+public class Bishop extends ChessPiece {
+    private String pozicija;
+    private Color boja;
+
+    Bishop(String pozicija, Color boja){
+        super(pozicija,boja);
+    }
+
+    public void move(String position) throws IllegalChessMoveException {
+        String stara = pozicija.toLowerCase();
+        String nova = position.toLowerCase();
+        if (!isPositionLegal(position))
+            throw new IllegalArgumentException("Illegal position");
+        boolean hasMove=false;
+        for(int i=-8;i<=8;i++)
+            for(int j=-8;j<=8;j++) {
+                if((i==0 && j==0) || (Math.abs(i) != Math.abs(j)) ) continue;
+                if( nova.charAt(0) == stara.charAt(0) + i && nova.charAt(1) == stara.charAt(1) + j ){
+                    hasMove=true;
+                    break;
+                }
+            }
+        if(!hasMove)
+            throw new IllegalChessMoveException("Illegal move for Queen");
+        pozicija=position;
+    }
+}
