f59dbd9a9cf7d45eb99733345c30384ac7a5d426
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-07 18:06:02 +0100

    isPositionLegal metoda za ChessPiece, modificiran konstruktor

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -5,5 +5,17 @@ public abstract class ChessPiece {
     public abstract String getPosition();
     public abstract Color getColor();
     public abstract void move(String position);
-    ChessPiece(String pozicija, Color boja){}
+    private String pozicija;
+    private Color boja;
+
+    public boolean isPositionLegal(String position){
+        if(position.length()!=2 || !((position.charAt(0)>='A' && position.charAt(0)<='H') || (position.charAt(0)>='a' && position.charAt(0)<='h') ) || !(position.charAt(1)>='1' && position.charAt(1)<='8') ) return false;
+        return true;
+    }
+
+    ChessPiece(String pozicija, Color boja){
+        if(!isPositionLegal(pozicija)) throw new IllegalArgumentException("Ilegalna pozicija");
+        this.pozicija=pozicija;
+        this.boja=boja;
+    }
 }
