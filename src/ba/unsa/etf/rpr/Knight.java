88171a4fcf66f8510d531f534458c9aef8158132
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-08 00:24:38 +0100

    Implementirana klasa Knight

---
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr;
+
+public class Knight extends ChessPiece {
+    private String pozicija;
+    private Color boja;
+
+    Knight(String pozicija, Color boja){
+        super(pozicija,boja);
+    }
+
+    public void move(String position) throws IllegalChessMoveException {
+        String stara = pozicija.toLowerCase();
+        String nova = position.toLowerCase();
+        if (!isPositionLegal(position))
+            throw new IllegalArgumentException("Illegal position");
+        boolean hasMove=false;
+        for(int i=-2;i<=2;i=i+4)
+            for(int j=-1;j<=1;j=j+2) {
+                if( nova.charAt(0) == stara.charAt(0) + i && nova.charAt(1) == stara.charAt(1) + j ){ //Provjera da li je data pozicija udaljena 2 mjesta horizontalno i jedno vertikalno
+                    hasMove=true;
+                    break;
+                }
+                if( nova.charAt(0) == stara.charAt(0) + j && nova.charAt(1) == stara.charAt(1) + i ){
+                    hasMove=true;
+                    break;
+                }
+            }
+        if(!hasMove)
+            throw new IllegalChessMoveException("Illegal move for Knight");
+        pozicija=position;
+    }
+}
