2014dd98b872ae7388b3d6c76fd608f0049bea97
Matej <mtalirevic1@etf.unsa.ba>
2018-11-08 16:10:30 +0100

    Reformatirane klase King, Knight, Pawn

--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -4,8 +4,8 @@ public class Knight extends ChessPiece {
     private String pozicija;
     private Color boja;
 
-    Knight(String pozicija, Color boja){
-        super(pozicija,boja);
+    Knight(String pozicija, Color boja) {
+        super(pozicija, boja);
     }
 
     public void move(String position) throws IllegalChessMoveException {
@@ -13,20 +13,20 @@ public class Knight extends ChessPiece {
         String nova = position.toLowerCase();
         if (!isPositionLegal(position))
             throw new IllegalArgumentException("Illegal position");
-        boolean hasMove=false;
-        for(int i=-2;i<=2;i=i+4)
-            for(int j=-1;j<=1;j=j+2) {
-                if( nova.charAt(0) == stara.charAt(0) + i && nova.charAt(1) == stara.charAt(1) + j ){ //Provjera da li je data pozicija udaljena 2 mjesta horizontalno i jedno vertikalno
-                    hasMove=true;
+        boolean hasMove = false;
+        for (int i = -2; i <= 2; i = i + 4)
+            for (int j = -1; j <= 1; j = j + 2) {
+                if (nova.charAt(0) == stara.charAt(0) + i && nova.charAt(1) == stara.charAt(1) + j) { //Provjera da li je data pozicija udaljena 2 mjesta horizontalno i jedno vertikalno
+                    hasMove = true;
                     break;
                 }
-                if( nova.charAt(0) == stara.charAt(0) + j && nova.charAt(1) == stara.charAt(1) + i ){
-                    hasMove=true;
+                if (nova.charAt(0) == stara.charAt(0) + j && nova.charAt(1) == stara.charAt(1) + i) {
+                    hasMove = true;
                     break;
                 }
             }
-        if(!hasMove)
+        if (!hasMove)
             throw new IllegalChessMoveException("Illegal move for Knight");
-        pozicija=position;
+        pozicija = position;
     }
 }
