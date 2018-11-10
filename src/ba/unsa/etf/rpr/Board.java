e61565f44e5a0a6a3adcf113566b8886c2560eab
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-10 11:12:50 +0100

    Metoda isCheck, radi nekad

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -92,7 +92,7 @@ public class Board {
         int staraY = posljednja.charAt(0) - 17 - '0';
         int staraX = posljednja.charAt(1) - '0' - 1;
 
-        if (sahovnica[novaX][novaY] != null && sahovnica[novaX][novaY].getColor() == figura.getColor())
+        /*if (sahovnica[novaX][novaY] != null && sahovnica[novaX][novaY].getColor() == figura.getColor())
             throw new IllegalChessMoveException("Illegal move");
 
         if (!(figura instanceof Knight)) {
@@ -117,20 +117,105 @@ public class Board {
             if (deltaX > deltaY) duzina = deltaX;
             else duzina = deltaY;
 
-            for (int i = 1; i < duzina - 1; i++) {
+            for (int i = 1; i < duzina; i++) {
                 if (sahovnica[staraX + i * dirX][staraY + i * dirY] != null) {
                     throw new IllegalChessMoveException("Illegal move");
                 }
             }
-        }
+        }*/
 
-        figura.move(newPosition);
-        sahovnica[novaX][novaY] = figura;
-        // figura=null;
-        sahovnica[staraX][staraY] = null;
+        if(!isMoveLegal(newPosition,figura)) throw new IllegalChessMoveException("Illegal move");
+
+        if(!(sahovnica[novaX][novaY] instanceof King)) {
+            figura.move(newPosition);
+            sahovnica[novaX][novaY] = figura;
+            sahovnica[staraX][staraY] = null;
+        }
+        else{
+
+        }
     }
 
+    public boolean isMoveLegal(String newPosition, ChessPiece figura) {
+
+
+        String lastPosition = figura.getPosition();
+        String posljednja = lastPosition.toUpperCase();
+        try {
+            figura.move(newPosition);
+        }
+        catch (IllegalChessMoveException e){
+            return false;
+        }
+        figura.setPosition(lastPosition);
+
+        String pozicija = newPosition.toUpperCase();
+        int novaY = pozicija.charAt(0) - 17 - '0';
+        int novaX = pozicija.charAt(1) - '0' - 1;
+        int staraY = posljednja.charAt(0) - 17 - '0';
+        int staraX = posljednja.charAt(1) - '0' - 1;
+
+        if (sahovnica[novaX][novaY] != null && sahovnica[novaX][novaY].getColor() == figura.getColor())
+            return false;
+
+        if (!(figura instanceof Knight)) {
+
+            int dirX = 0, dirY = 0;
+
+            if (figura instanceof Pawn && staraY != novaY && sahovnica[novaX][novaY] == null) {
+                return false;
+            }
+
+            if (novaX > staraX) dirX = 1;
+            else if (novaX == staraX) dirX = 0;
+            else dirX = -1;
+
+            if (novaY > staraY) dirY = 1;
+            else if (novaY == staraY) dirY = 0;
+            else dirY = -1;
+
+            int duzina = 0;
+            int deltaX = Math.abs(staraX - novaX);
+            int deltaY = Math.abs(staraY - novaY);
+            if (deltaX > deltaY) duzina = deltaX;
+            else duzina = deltaY;
+
+            for (int i = 1; i < duzina; i++) {
+                if (sahovnica[staraX + i * dirX][staraY + i * dirY] != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
     boolean isCheck(ChessPiece.Color color) {
+        ChessPiece kralj=null;
+
+        boolean stani=false;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (sahovnica[i][j] != null && (sahovnica[i][j] instanceof King) && sahovnica[i][j].getColor() == color) {
+                    kralj = sahovnica[i][j];
+                    stani = true;
+                    break;
+                }
+
+            }
+            if (stani) break;
+        }
+
+        String kingPosition=kralj.getPosition();
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (sahovnica[i][j] != null && sahovnica[i][j].getColor() != color && isMoveLegal(kingPosition,sahovnica[i][j])){
+                    return true;
+                }
+
+            }
+        }
         return false;
     }
 }
