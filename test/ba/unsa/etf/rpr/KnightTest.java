8eb896f1602406da616e01669f93acc9756d96f3
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-10 16:08:53 +0100

    Testovi za klase Knight, Pawn

--- test/ba/unsa/etf/rpr/KnightTest.java
+++ test/ba/unsa/etf/rpr/KnightTest.java
@@ -14,4 +14,37 @@ class KnightTest {
         );
 
     }
+
+    @org.junit.jupiter.api.Test
+    void constructor1() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Knight("I2", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void constructor2() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Knight("B9", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void constructor3() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Knight("", ChessPiece.Color.WHITE)
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveIllegal1() {
+        Knight k = new Knight("C1", ChessPiece.Color.BLACK);
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> k.move("C0")
+        );
+    }
 }
--- test/ba/unsa/etf/rpr/PawnTest.java
+++ test/ba/unsa/etf/rpr/PawnTest.java
@@ -12,4 +12,46 @@ class PawnTest {
         );
     }
 
+    @org.junit.jupiter.api.Test
+    void move11() {
+        Pawn p = new Pawn("A2", ChessPiece.Color.WHITE);
+        assertDoesNotThrow(
+                () -> p.move("A3")
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void move3() {
+        Pawn p = new Pawn("C2", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> p.move("C5")
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void move22() {
+        Pawn p = new Pawn("H2", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> {
+                    p.move("H4");
+                    p.move("H6");
+                }
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveBack() {
+        Pawn p = new Pawn("C2", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> p.move("C1")
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveSide() {
+        Pawn p = new Pawn("C2", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> p.move("D2")
+        );
+    }
 }
